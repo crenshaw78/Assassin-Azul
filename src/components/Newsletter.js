eb18b77339ca0c7ef17fcf26518ff028848a2887
Chris Renshaw <chrisrenshaw@Chriss-MacBook-Pro.local>
2021-12-07 19:45:42 -0600

    submitting work

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 
 function Newsletter() {
+  const [isNewsLetterOpen, toggleNewsletterState] = useState(false)
   return (
     <div className="s-bkgd flex-center padding-2">
       <h1>JOIN NEWSLETTER</h1>
       <form>
-        <input type="text" name="name" value="Email Address" onSelect="handleNewsletter()" />
+        <input type="text" name="name" value="Email Address" onSelect="toggleNewsletterState() => 
+        isNewsLetterOpen = true;" />
         <input type="submit" value="Submit" />
       </form>
     </div>
